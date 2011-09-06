50670097900747a8090dc640a223ecba7ee54de4
Iuri Diniz <iuridiniz@gmail.com>
2011-09-06 14:35:24 -0300

    using constants to set HOST and PORT

--- stomp_proxy.py
+++ stomp_proxy.py
@@ -4,10 +4,14 @@ from twisted.internet import reactor
 from twisted.protocols.portforward import ProxyFactory
 from websocket import WebSocketProtocol, WebSocketFactory
 
+
+#HOST, PORT = "e1.acom.evolux.digi.com.br", 61613
+HOST, PORT = "localhost", 61613
+
 def main():
     log.startLogging(sys.stdout)
-
-    stomp_proxy_factory = ProxyFactory("localhost", 61613)
+    
+    stomp_proxy_factory = ProxyFactory(HOST, PORT)
     stomp_ws_factory = WebSocketFactory(stomp_proxy_factory)
 
     reactor.listenTCP(9090, stomp_proxy_factory)
